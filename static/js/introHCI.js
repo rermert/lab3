f47bdd7f16ea95882acda01146510b312c411d65
rermert <rermert@ucsd.edu>
2017-01-25 10:03:02 -0800

    Lab 3 working

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event  
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $("#jumbotron h1");    
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    $(description).toggle();
+    /*
+    if (description.length == 0) {
+       $("#pdesc").show()
+    } else {
+       $("#pdesc").hide()
+    }
+    */
 }
